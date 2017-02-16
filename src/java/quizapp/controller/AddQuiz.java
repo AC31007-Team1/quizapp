8e0408e83fe5e97622291943eee83ae7b6a68b44
ijm7 <ijmorton500@gmail.com>
2017-02-16 19:03:10 +0000

    begun designing createquiz

---
+++ src/java/quizapp/controller/AddQuiz.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizapp.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "QuizQuestions", urlPatterns = {"/QuizQuestions", "/QuizQuestions/"})
+public class QuizQuestions extends HttpServlet{
+    public QuizQuestions() {
+}
+/**
+ *
+ * @author iain
+ */
+public class AddQuiz {
+    
+}
