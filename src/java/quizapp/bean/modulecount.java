56f4aff56efb61e9eb7bb9d468d447a95f9119cd
ijm7 <ijmorton500@gmail.com>
2017-02-20 22:30:42 +0000

    created moduleamount bean

---
+++ src/java/quizapp/bean/modulecount.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizapp.bean;
+
+/**
+ *
+ * @author Iain
+ */
+public class modulecount {
+    int moduleamount;
+    public void setModuleAmount(int moduleamount) {
+        this.moduleamount=moduleamount;
+    }
+    
+    public int getModuleID() {
+        return moduleamount;
+    }
+}
