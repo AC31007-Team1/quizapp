7081ddbe6c8581c1f6322fd4e6d16d988d636a1b
Craigieboy10 <craig-watt@live.co.uk>
2017-02-24 17:43:16 +0000

    Staff View Quiz for delete and change availability.

--- src/java/quizapp/model/DeleteStaffQuiz.java
+++ src/java/quizapp/model/DeleteStaffQuiz.java
@@ -54,4 +54,51 @@ public class DeleteStaffQuiz {
         }
         return true;
     }
+    
+    public boolean changeAvailability(int quizID) {
+
+        String driverName = "com.mysql.jdbc.Driver";
+        String connectionUrl = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/";
+        String dbName = "16agileteam1db";
+        String userID = "16agileteam1";
+        String password = "8320.at1.0238";
+        int currentA =0;
+        
+        
+        
+        
+
+        Connection connection = null;
+
+        String query1 = "SELECT quiz_available FROM 16agileteam1db.quiz WHERE quiz_id = " +quizID;
+        
+
+        try {
+            connection = DriverManager.getConnection(connectionUrl + dbName, userID, password);
+            Statement statement = connection.createStatement();
+
+            ResultSet rs = statement.executeQuery(query1);
+            while(rs.next()) {
+                currentA = rs.getInt("quiz_available");
+            }
+            if(currentA == 1){
+                currentA = 0;
+            }
+            else{
+                currentA = 1;
+            }
+            String query2 = "UPDATE 16agileteam1db.quiz SET quiz_available = " + currentA + " WHERE quiz_id = " + quizID;
+            statement.execute(query2);
+            connection.close();
+            
+
+        } catch (SQLException e) {
+            e.getMessage();
+            return false;
+        }
+        
+        
+        //String query2 = "UPDATE 16agileteam1db.quiz SET quiz_available = " + newA + " WHERE quiz_id = " + quizID;
+        return true;
+    }
 }
