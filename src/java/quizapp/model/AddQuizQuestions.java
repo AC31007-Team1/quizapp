ff6e424b8487dd8331334b7e691d8665822f76b3
Anthony Grimes <contact@anthonygrimes.co.uk>
2017-03-09 19:39:25 +0000

    code formatting and redundant variables

--- src/java/quizapp/model/AddQuizQuestions.java
+++ src/java/quizapp/model/AddQuizQuestions.java
@@ -1,11 +1,8 @@
 package quizapp.model;
 
 import java.sql.*;
-import quizapp.bean.*;
 
 public class AddQuizQuestions {
-    public AddQuizQuestions() {
-    }
 
     public void submitQuestion(String question, String quizid) {
         String driverName = "com.mysql.jdbc.Driver";
@@ -22,25 +19,19 @@ public class AddQuizQuestions {
 
 
         Connection connection = null;
-        Statement statement = null;
 
-
-        // first value set as 1 as quiz_id is one, this will be gathered before hand once quizzes are dynamic
         try {
             connection = DriverManager.getConnection(connectionUrl + dbName, userID, password);
-            statement = connection.createStatement();
             
-            
-            String query = "INSERT INTO quiz_questions(quiz_id, question)" + "VALUES(?, ?)"; // my sql statement
+            String query = "INSERT INTO quiz_questions(quiz_id, question)" + "VALUES(?, ?)";
 
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, quizid); // update with quiz_id
+            preparedStatement.setString(1, quizid);
             preparedStatement.setString(2, question);
 
             preparedStatement.execute();
 
             connection.close();
-
 
         } catch (Exception e) {
             e.getMessage();
@@ -48,7 +39,6 @@ public class AddQuizQuestions {
     }
 
 
-    //possible overlap if questions entered at same time? and must also grab quiz ID at some point to be accurate
     public void submitAnswers(String answerc, String[] answeri, String quizid) {
         String driverName = "com.mysql.jdbc.Driver";
         String connectionUrl = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/";
@@ -84,12 +74,12 @@ public class AddQuizQuestions {
             String query = "INSERT INTO quiz_answers(quiz_question_id, quiz_id, correct_answer, incorrect_answer_one, incorrect_answer_two, incorrect_answer_three)" + " VALUES(?, ?, ?, ?, ?, ?);"; // my sql statement
 
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, ID); // update with quizquestionid
+            preparedStatement.setInt(1, ID);
             preparedStatement.setString(2, quizid);
             preparedStatement.setString(3, answerc);
 
             for(int i = 0; i < 3; i++) {
-                preparedStatement.setString(i+4, answeri[i]); //lol this logic, somebody fire me, ah doesn't work as only one incorrect_answer row
+                preparedStatement.setString(i+4, answeri[i]);
             }
             preparedStatement.execute();
 
