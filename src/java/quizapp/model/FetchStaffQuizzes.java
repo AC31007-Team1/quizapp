dd1ec053b0607573c442f61a1867a632b34d8443
Anthony Grimes <contact@anthonygrimes.co.uk>
2017-03-12 18:05:08 +0000

    fetch staff quizzes update

--- src/java/quizapp/model/FetchStaffQuizzes.java
+++ src/java/quizapp/model/FetchStaffQuizzes.java
@@ -1,45 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quizapp.model;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.LinkedList;
 import quizapp.bean.StaffQuiz;
-
+import quizpp.util.DatabaseManager;
 
 public class FetchStaffQuizzes {
+    
+    private DatabaseManager db = new DatabaseManager();
 
     public LinkedList<StaffQuiz> getQuizzes(int staffID) {
-        String driverName = "com.mysql.jdbc.Driver";
-        String connectionUrl = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/";
-        String dbName = "16agileteam1db";
-        String userID = "16agileteam1";
-        String password = "8320.at1.0238";
+        
+        LinkedList<StaffQuiz> quizListForReturn = new LinkedList<StaffQuiz>();
+        String selectQuizQuery = "SELECT quiz_id, quiz_name, quiz_available, quiz_added FROM 16agileteam1db.quiz WHERE staff_id_number=" + staffID;
 
         int qid = 0;
         String qn = "sql error";
         int qa = 0;
-        //timestamp
         
-        try {
-            Class.forName(driverName);
-        } catch (ClassNotFoundException e) {
-        }
-        //java.sql.timestamp
-        Connection connection = null;
-        LinkedList<StaffQuiz> quizListForReturn = new LinkedList<StaffQuiz>();
-        String selectQuizQuery = "SELECT quiz_id, quiz_name, quiz_available, quiz_added FROM 16agileteam1db.quiz WHERE staff_id_number=" + staffID;
-        try {
-            connection = DriverManager.getConnection(connectionUrl + dbName, userID, password);
-            Statement statement = connection.createStatement();
+        try(Connection connection = db.getConnection(); Statement statement = connection.createStatement()) {
+            
             ResultSet rs = statement.executeQuery(selectQuizQuery);
+            
             while(rs.next()) {
                 qid = rs.getInt("quiz_id");
                 qn = rs.getString("quiz_name");
@@ -47,9 +32,11 @@ public class FetchStaffQuizzes {
                 StaffQuiz tempQuiz = new StaffQuiz(qid,qn,qa);
                 quizListForReturn.add(tempQuiz);
             }
+            
             connection.close();
-        } catch (SQLException e) {
-            e.getMessage();
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
         return quizListForReturn;
     }
