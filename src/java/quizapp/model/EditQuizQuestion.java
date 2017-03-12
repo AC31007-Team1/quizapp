6198fc7322f70e33c8cc5f5d8ba0123983b32461
Anthony Grimes <contact@anthonygrimes.co.uk>
2017-03-12 17:41:13 +0000

    update edit of questions

--- src/java/quizapp/model/EditQuizQuestion.java
+++ src/java/quizapp/model/EditQuizQuestion.java
@@ -1,109 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quizapp.model;
 
 import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.util.Calendar;
-import javax.servlet.annotation.WebServlet;
-
-/**
- *
- * @author iain
- */
+import quizpp.util.DatabaseManager;
 
 public class EditQuizQuestion {
     
-    public boolean updateQuizQuestions(String question, int questionID) throws SQLException, ParseException {
+    private DatabaseManager db = new DatabaseManager();
+    private boolean status = false;
+    
+    public boolean updateQuizQuestions(String question, int questionID) {
         
-        //hide userID & password (shippable?)
-        /*String connectionUrl = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/";
-        String dbName = "16agileteam1db";
-        String userID = "16agileteam1";
-        String password = "8320.at1.0238";*/
+        String query = "UPDATE quiz_questions SET question=? " + "WHERE quiz_question_id = ?";
         
-        String driverName = "com.mysql.jdbc.Driver";
-        String connectionUrl = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/";
-        String dbName = "16agileteam1db";
-        String userID = "16agileteam1";
-        String password = "8320.at1.0238";
-        
-        try {
-            Class.forName(driverName);
-        } catch (ClassNotFoundException e) {
-        }
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(connectionUrl + dbName, userID, password);
+        try(Connection connection = db.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             
-            PreparedStatement pstmt = connection.prepareStatement(
-                    "UPDATE quiz_questions SET question=? " + "WHERE quiz_question_id = ?");
-           
-            pstmt.setString( 1, question );
-            pstmt.setInt( 2, questionID );
+            preparedStatement.setString( 1, question );
+            preparedStatement.setInt( 2, questionID );
             
+            preparedStatement.executeUpdate();
             
-            
-            pstmt.executeUpdate();
+            status = true;
             
             connection.close();
-            return true;
-        } catch (SQLException e) {
-            e.getMessage();
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            status = false;
+            e.printStackTrace();
         }
-        return false;
+        return status;
     }
     
-    public boolean updateQuizAnswers(String correctAnswer, String incorrectAnswer1, String incorrectAnswer2, String incorrectAnswer3, String explanation, int questionID) throws SQLException, ParseException {
+    public boolean updateQuizAnswers(String correctAnswer, String incorrectAnswer1, String incorrectAnswer2, String incorrectAnswer3, String explanation, int questionID) {
         
-        //hide userID & password (shippable?)
-        /*String connectionUrl = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/";
-        String dbName = "16agileteam1db";
-        String userID = "16agileteam1";
-        String password = "8320.at1.0238";*/
+        String query = "UPDATE quiz_answers SET correct_answer=?, incorrect_answer_one=?, incorrect_answer_two=?, incorrect_answer_three=?, answer_ex=?"
+                + "WHERE quiz_question_id = ?";
         
-        String driverName = "com.mysql.jdbc.Driver";
-        String connectionUrl = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/";
-        String dbName = "16agileteam1db";
-        String userID = "16agileteam1";
-        String password = "8320.at1.0238";
-        
-        try {
-            Class.forName(driverName);
-        } catch (ClassNotFoundException e) {
-        }
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(connectionUrl + dbName, userID, password);
+        try(Connection connection = db.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             
-            PreparedStatement pstmt = connection.prepareStatement(
-            "UPDATE quiz_answers SET correct_answer=?, incorrect_answer_one=?, incorrect_answer_two=?, incorrect_answer_three=?, answer_ex=?"+ "WHERE quiz_question_id = ?");
-            pstmt.setString( 1, correctAnswer );
-            pstmt.setString( 2, incorrectAnswer1 );
-            pstmt.setString( 3, incorrectAnswer2 );
-            pstmt.setString( 4, incorrectAnswer3 );
-            pstmt.setString( 5, explanation);
-            pstmt.setInt( 6, questionID );
+            preparedStatement.setString( 1, correctAnswer );
+            preparedStatement.setString( 2, incorrectAnswer1 );
+            preparedStatement.setString( 3, incorrectAnswer2 );
+            preparedStatement.setString( 4, incorrectAnswer3 );
+            preparedStatement.setString( 5, explanation);
+            preparedStatement.setInt( 6, questionID );
             
+            preparedStatement.executeUpdate();
             
-            pstmt.executeUpdate();
+            status = true;
             
             connection.close();
-            return true;
-        } catch (SQLException e) {
-            e.getMessage();
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            status = false;
+            e.printStackTrace();
         }
-        return false;
+        return status;
     }
 }
