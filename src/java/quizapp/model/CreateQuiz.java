79c362bc460ab1d0b0bcfa8817e9750599d66408
Anthony Grimes <contact@anthonygrimes.co.uk>
2017-03-12 16:54:42 +0000

    AddQuiz update

--- src/java/quizapp/model/CreateQuiz.java
+++ src/java/quizapp/model/CreateQuiz.java
@@ -1,57 +1,47 @@
 package quizapp.model;
-import java.sql.*;
-import java.text.ParseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import quizpp.util.DatabaseManager;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Calendar;
-/**
- *
- * @author iain
- */
+
 public class CreateQuiz {
     
-    public boolean insertQuiz(String quizname, int module, int staffID, LocalDateTime created, int available) throws SQLException, ParseException {
+    private DatabaseManager db = new DatabaseManager();
+    private boolean status = false;
+    
+    public boolean insertQuiz(String quizname, int module, int staffID, LocalDateTime created, int available) {
+        
+        String query = "INSERT INTO quiz ( staff_id_number, module_id, quiz_name, quiz_available, quiz_added ) " +
+            " values (?, ?, ?, ?, ? )";
+        
         SimpleDateFormat sdf = new SimpleDateFormat( "yyyy/MM/dd HH:mm:ss" );  // United States style of format.
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         sdf.format(timestamp);
-        //hide userID & password (shippable?)
-        /*String connectionUrl = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/";
-        String dbName = "16agileteam1db";
-        String userID = "16agileteam1";
-        String password = "8320.at1.0238";*/
         
-        String driverName = "com.mysql.jdbc.Driver";
-        String connectionUrl = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/";
-        String dbName = "16agileteam1db";
-        String userID = "16agileteam1";
-        String password = "8320.at1.0238";
         
-        try {
-            Class.forName(driverName);
-        } catch (ClassNotFoundException e) {
-        }
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(connectionUrl + dbName, userID, password);
+        try(Connection connection = db.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             
-            PreparedStatement pstmt = connection.prepareStatement(
-            "INSERT INTO quiz ( staff_id_number, module_id, quiz_name, quiz_available, quiz_added ) " +
-            " values (?, ?, ?, ?, ? )");
-            pstmt.setInt( 1, staffID );
-            pstmt.setInt( 2, module ); 
-            pstmt.setString( 3, quizname );
-            pstmt.setInt( 4, available );
-            java.sql.Date timeNow = new Date(Calendar.getInstance().getTime().getTime());
-            //java.sql.Date sqlDate = new java.sql.Date( myDate.getTime() ); // Notice the ".sql." (not "util") in package name.
-            pstmt.setTimestamp( 5, timestamp ); 
+            preparedStatement.setInt( 1, staffID );
+            preparedStatement.setInt( 2, module ); 
+            preparedStatement.setString( 3, quizname );
+            preparedStatement.setInt( 4, available );
+            preparedStatement.setTimestamp( 5, timestamp ); 
             
-            pstmt.executeUpdate();
+            preparedStatement.executeUpdate();
+            
+            status = true;
             
             connection.close();
-            return true;
-        } catch (SQLException e) {
-            e.getMessage();
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            status = false;
+            e.printStackTrace();
         }
-        return false;
+        
+        return status;
     }
 }
