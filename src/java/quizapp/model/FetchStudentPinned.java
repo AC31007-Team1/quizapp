9c9317d73f366717c00bd0906045fc706d04b858
Anthony Grimes <contact@anthonygrimes.co.uk>
2017-03-12 18:08:48 +0000

    fetch stu pinned update

--- src/java/quizapp/model/FetchStudentPinned.java
+++ src/java/quizapp/model/FetchStudentPinned.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quizapp.model;
 
 import java.sql.Connection;
@@ -10,35 +5,25 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import quizapp.bean.Pinned;
+import quizpp.util.DatabaseManager;
 
 public class FetchStudentPinned {
+    
+    private DatabaseManager db = new DatabaseManager();
 
     public LinkedList<Pinned> getQuizzes(int studentID) {
-        String driverName = "com.mysql.jdbc.Driver";
-        String connectionUrl = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/";
-        String dbName = "16agileteam1db";
-        String userID = "16agileteam1";
-        String password = "8320.at1.0238";
 
         String qid;
         String qn = "sql error";
-        //timestamp
-
-        try {
-            Class.forName(driverName);
-        } catch (ClassNotFoundException e) {
-        }
-        //java.sql.timestamp
-        LinkedList<Pinned> pinnedListForReturn = new LinkedList<Pinned>();
-        Connection connection = null;
+        
         LinkedList<String> quizID = new LinkedList<String>();
+        LinkedList<Pinned> pinnedListForReturn = new LinkedList<Pinned>();
         String selectQuizQuery = "SELECT quiz_id FROM 16agileteam1db.stu_fav WHERE matriculation_number=" + studentID;
-        try {
-            connection = DriverManager.getConnection(connectionUrl + dbName, userID, password);
-            Statement statement = connection.createStatement();
+        
+        try(Connection connection = db.getConnection(); Statement statement = connection.createStatement()) {
+            
             ResultSet rs = statement.executeQuery(selectQuizQuery);
             while (rs.next()) {
                 qid = rs.getString("quiz_id");
@@ -53,10 +38,11 @@ public class FetchStudentPinned {
                 Pinned tempPinned = new Pinned(quizID.get(i), quizName.get(i));
                 pinnedListForReturn.add(tempPinned);
             }
-
+            
             connection.close();
-        } catch (SQLException e) {
-            e.getMessage();
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
         return pinnedListForReturn;
     }
@@ -65,29 +51,21 @@ public class FetchStudentPinned {
 
         LinkedList quizName = new LinkedList<>();
 
-        String driverName = "com.mysql.jdbc.Driver";
-        String connectionUrl = "jdbc:mysql://silva.computing.dundee.ac.uk:3306/";
-        String dbName = "16agileteam1db";
-        String userID = "16agileteam1";
-        String password = "8320.at1.0238";
-        Connection connection = null;
-
-        try {
-            connection = DriverManager.getConnection(connectionUrl + dbName, userID, password);
-            Statement statement = connection.createStatement();
+        try(Connection connection = db.getConnection(); Statement statement = connection.createStatement()) {
+            
             for (int i = 0; i < quizID.size(); i++) {
                 String selectQuizQuery = "SELECT quiz_name FROM 16agileteam1db.quiz WHERE quiz_id=" + quizID.get(i);
                 ResultSet rs = statement.executeQuery(selectQuizQuery);
                 while (rs.next()) {
                     quizName.add(rs.getString("quiz_name"));
-                }
+                } 
             }
-
+            
             connection.close();
-        } catch (SQLException e) {
-            e.getMessage();
-        }
-        
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }   
         return quizName;
     }
 }
